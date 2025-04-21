7d7d73ac338121c7b9311085ca7b12974c6d3b6d
AY06M <aibnelhabib@gmail.com>
2025-04-21 15:59:37 +0100

    Add files via upload

--- tp_piles.c
+++ tp_piles.c
@@ -47,6 +47,47 @@ void afficher(noeud**p){
         }
     }
 }
+int taille(noeud**p){
+    noeud *temp=*p;
+    int i=0;
+    if(temp==NULL){
+        printf("Erreur \n");
+        return 0;
+    }
+    else{
+        
+        while(temp!=NULL){
+            i++;
+            temp=temp->prec;
+        }
+    }
+    return i;
+}
+void inserermillieu(noeud **p, int d, int pos){
+    noeud *temp=*p;
+    noeud *nv=NULL;
+    nv=(noeud *)malloc(sizeof(noeud));
+    if(nv==NULL){
+        printf("Erreur \n");
+        return;
+    }
+    else{
+        nv->d=d;
+        if(pos==0){
+            nv->prec=*p;
+            *p=nv;
+        }
+        else{
+            for(int i=0;i<pos-1 && temp!=NULL;i++){
+                temp=temp->prec;
+            }
+            if(temp!=NULL){
+                nv->prec=temp->prec;
+                temp->prec=nv;
+            }
+        }
+    }
+}
 void main(){
     noeud *p=NULL;
     printf("empilement:\n");
@@ -54,8 +95,13 @@ void main(){
     empiler(&p,20);
     empiler(&p,30);
     afficher(&p);
+    printf("taille: %d\n",taille(&p));
     printf("depilement:\n");
     depiler(&p);
+    afficher(&p);
+    printf("taille: %d\n",taille(&p));
+    inserermillieu(&p,40,1);
+    printf("inserer millieu:\n");
     afficher(&p);
 }
 
